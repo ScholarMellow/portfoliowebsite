ae658fa66b3908af879ae68e98b18e67c3317c9a
ScholarMellow <bermudezandrew9@gmail.com>
2018-08-25 03:47:08 -0400

    Revert "first commit"
    This reverts commit 225d651051554e24a148078e1cf795966a4b23db.

--- contact.js
+++ contact.js
@@ -1,19 +1,8 @@
-
-
 $(".contacts").on("click", function(event){
     $('#god').fadeOut('slow', function(){
-        /*$('#god').remove();*/
-        $('#contactpage').fadeIn(500);
+        $('#god').remove();
+        $('#contact').fadeIn(500);
         $('body').css('background-color', 'rgb(112, 163, 244)');
-        window.scrollTo(0, 0);
     });
     event.stopPropagation();
-});
-
-$("#buttonfadetoggler").on("click", function(event) {
-	$("#contactpage").fadeOut('slow', function(){
-		$("#god").fadeIn(500);
-		$("body").css("background-color", "white");
-	});
-	window.scrollTo(0, 0);
 });
